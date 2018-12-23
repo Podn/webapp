14759c68cc10794e24b6ed150f8e45cae0834d62
dgriff03 <daniel.griffin@tufts.edu>
2018-12-23 12:58:37 -0800

    update homepage to new mocks. add fields to user, introduce podcast and roles table. Make sign-up error messaging nicer.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,27 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         user_saved = @user.save
         if !user_saved
-            redirect_to '/signup', flash: {notice: "Error creating account, please try again."} 
+            redirect_to '/signup', flash: {notice: "Error creating account, please try again.\n#{formatErrors(@user.errors.messages)}"} 
             return
         end
         render :json => {saved: user_saved, user: @user} # don't do msg.to_json
+    end
+
+    def information
     end
 
 
     def user_params
         params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
+
+    private
+      def formatErrors(errors) 
+        values = ""
+        errors.each do |field, error|
+            errors = error.join(" and ")
+            values += "#{field} #{errors}.\n"
+        end
+        return values
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :users
+  root 'main#index'
   get 'main/index'
+  get '/signup/information', to: 'users#information'
   get '/how-it-works', to: 'main#how_it_works'
   get '/signup', to: 'main#signup', as: :signup
-  root 'main#index'
   get '/login', to: 'main#login', as: :login
   post '/login', to: 'main#login_attempt', as: :login_attempt
   get '/logout', to: 'main#logout', as: :logout
